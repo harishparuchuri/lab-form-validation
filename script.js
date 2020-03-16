ca21a3944beb59d0a4d6eb8ba8b4fe542994ab62
harishparuchuri <harishparuchuri007@gmail.com>
2020-03-16 10:00:32 +0530

    form validation

--- script.js
+++ script.js
@@ -8,6 +8,29 @@
 // username should not contain special characters
 // the function should return true if all the conditions above are validated.
 // return false if the validation fails
+function checkUserName() {
+    let username = document.getElementsByClassName('username');
+    if (username.length == 0)
+    {
+        alert("Name can't be blank");
+        return false;
+    }
+        
+    else if ((username.length >= 8) && (username.length < 15))
+    {
+        alert("name must be >8 and <15 char");
+        return false;
+    }
+    else 
+    {
+        for (var i = 0; i < username.length; i++)
+        if ((username[i] == " ") || (username[i] == "@") || (username[i] == "&") || (username[i] == "$") || (username[i] == "!"))
+            return false;
+    }
+        
+    
+   
+}
 
 // Progression 2 
 // Check mail
@@ -15,6 +38,23 @@
 // only @ , . , _ [at,dot,underscore] is allowed
 // It can be alphanumeric in nature
 // return true if it passses all validation and false otherwise
+function checkEmail() {
+    var mail = document.getElementsByClassName('mail');
+    if (mail == "" || mail == "null") {
+        alert("please enter your email");
+
+        return false;
+    }
+    var atpos = mail.indexOf("@");
+    var dotpos = mail.lastIndexOf(".");
+    if (atpos < 1 || dotpos < atpos + 2 || dotpos + 2 >= mail.length) {
+        alert("Not a valid e-mail address");
+        return false;
+    }
+    alert("Registration successfull");
+    return true;
+}
+
 
 // Progression 3
 // Check password
@@ -22,3 +62,24 @@
 // should be alphanumeric in nature
 // password must contain atleast one Uppercase, one number and special characters[!@#$%^&*()_]
 // return true else return false
+function checkPassword() {
+    let pass = document.getElementsByClassName('password');
+    var checkSpecial = /[*@!#%&]+/.test(pass);
+    var checkUpper = /[A-Z]+/.test(pass);
+    var checkLower = /[a-z]+/.test(pass);
+    if (pass == "" || pass == "null") {
+        alert("please enter ur password");
+
+        return false;
+    }
+    if (checkSpecial && checkLower && checkUpper) {
+        flag = 1;
+    }
+    if (flag == 0) {
+        alert("Password should be the combination of atleast one lowercase letter, uppercaseletter & special symbols like($, & ,#)");
+
+        return false;
+    }
+    alert("Registration successfull");
+    return true;
+}
